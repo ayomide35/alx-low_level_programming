fbd0f6b071dfbc040c4f694edd658f2cd3a700b5
ayomide35 <vhydolee4190@gmail.com>
2022-09-13 10:20:40 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,20 @@
 #include "main.h"
 
-
 /**
- * p	while (letter <= 'z')
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int round = 0;
+	char letter = 'a';
+
+	while (round < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
 		{
 			_putchar(letter);
 			letter++;
@@ -11,3 +23,4 @@
 
 		round++;
 	}
+}
