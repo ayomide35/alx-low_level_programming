a153428b35654c47c15ccf45638b7da6b26b9f9d
ayomide35 <vhydolee4190@gmail.com>
2022-09-15 08:12:11 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+
+#define HOLBERTON_H
+
+
 
 #include <stdio.h>
 
+
+
 void positive_or_negative(int i);
 
-#endif /* MAIN_H */
+int largest_number(int a, int b, int c);
+
+int convert_day(int month,int day);
+
+void print_remaining_days(int month,int day,int year);
+
+void print_to_98(int n);
+
+
+
+#endif
