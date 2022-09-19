7a0ecd3cb45cc6edf5fabf0e5c27eab0c99d3f34
ayomide35 <vhydolee4190@gmail.com>
2022-09-19 08:26:42 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - random password generator for 101-crackme
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	int randomizer = 0;
+
+	srand((unsigned int)(time(NULL)));
+	char numbers[] = "0123456789";
+	char letter[] = "abcdefghijklmnoqprstuvwyzx";
+	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+	char symbols[] = "!@#$^&*?";
+	char password[N];
+
+	randomizer = rand() % 4;
+	 for (i = 0; i < N; i++)
+	 {
+		 if (randomizer == 1)
+		 {
+			  password[i] = numbers[rand() % 10];
+			  randomizer = rand() % 4;
+			  printf("%c", password[i]);
+		 }
+		 else if (randomizer == 2)
+		 {
+			 password[i] = symbols[rand() % 8];
+			 randomizer = rand() % 4;
+			 printf("%c", password[i]);
+		 }
+		 else if (randomizer == 3)
+		 {
+			 password[i] = LETTER[rand() % 26];
+			 randomizer = rand() % 4;
+			 printf("%c", password[i]);\
+		 }
+		 else
+		 {
+			 password[i] = letter[rand() % 26];
+			 randomizer = rand() % 4;
+			 printf("%c", password[i]);
+		 }
+	 }
+}
