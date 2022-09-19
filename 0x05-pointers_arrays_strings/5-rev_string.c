ce246f4c64c6109b525a696fbbe53749a104742c
ayomide35 <vhydolee4190@gmail.com>
2022-09-19 06:36:43 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i, a, b;
+	char first, last;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	a = i - 1;
+	b = a / 2;
+	while (b >= 0)
+	{
+		first = s[a - b];
+		last = s[b];
+		s[b] = first;
+		s[a - b] = last;
+		b--;
+	}
+}
+
