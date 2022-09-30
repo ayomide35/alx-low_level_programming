efffa676dde4572cb541a680dd97d27262cc0316
ayomide35 <vhydolee4190@gmail.com>
2022-09-30 15:44:05 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  * argv: array of arguments
  * Return: 0
  */
-
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
