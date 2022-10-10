be7ba06f6f717ff788e7daa7578b4d4c128a6232
ayomide35 <vhydolee4190@gmail.com>
2022-10-10 15:19:34 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,11 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string
+ * Return: pointer to newly allocated space in memory
+ */
+
+
