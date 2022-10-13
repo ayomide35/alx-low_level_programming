c52760d30aca6b50ff127a6b338623101bf948da
ayomide35 <vhydolee4190@gmail.com>
2022-10-13 16:37:55 -0700

    0-sum_them_all.c

--- variadic_functions.h
+++ variadic_functions.h
@@ -2,23 +2,19 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-
+void print_all(const char * const format, ...);
 /**
  * struct op - Struct op
- *
- * @c: The data type (char)
+ * @op: format
  * @f: The function associated with it
  */
 
-typedef struct op
+typedef struct format
 {
-	char c;
-	void (*f)(va_list);
-} op_t;
-
-void print_all(const char * const format, ...);
+	char *op;
+	void (*f)(va_list all);
+} f;
 #endif
